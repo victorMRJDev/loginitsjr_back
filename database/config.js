2bfc4c306ad4a49593ce935c5baa0b47a6194a5a
victorMRJDev <victortecno98@gmail.com>
2024-10-04 02:00:11 -0600

    add mysql2

--- database/config.js
+++ database/config.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
+import mysql2 from 'mysql2';
 
 const db = new Sequelize( process.env.MYSQL_DB_NAME, '372013', 'itsj123',{
     host: process.env.MYSQL_URL,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    dialectModule: mysql2
+
 });
 
 module.exports = {
