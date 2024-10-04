09cf345dbde159661fef72210bd6cf5c46dfbc9f
victorMRJDev <victortecno98@gmail.com>
2024-10-04 02:50:04 -0600

    Correccion import#

--- controller/auth.js
+++ controller/auth.js
@@ -1,5 +1,5 @@
 const {response} = require('express');
-const {  User } = require('../models/user');
+const {  User } = require('../models/User');
 const bcrypt = require('bcryptjs');
 const {generateJWT} = require('../helpers/jwt');
 
